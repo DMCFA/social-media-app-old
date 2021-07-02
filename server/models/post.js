a00eb9d16047ad81d3982622fc820e24d82c2d37
Duarte Almeida <duartemalmeida@gmail.com>
2021-07-02 14:44:15 +0100

    initial commit

---
+++ server/models/post.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const postSchema = new Schema({
+	title: String,
+	author: String,
+	body: String,
+	comments: [{
+		body: String,
+		date: Date
+	}],
+	date: {
+		type: Date,
+		default: new Date ()
+	},
+	keywords: [String],
+	imageFile: String,
+	likes: {
+		type: Number,
+		default: 0
+	}
+})
+
+postSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id,
+		delete returnedObject.__v
+	}
+})
+
+export default mongoose.model('Post', postSchema)
