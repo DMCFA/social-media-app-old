1ccd2dff1b996977ec5c895cdc3996aabc165b32
Duarte Almeida <duartemalmeida@gmail.com>
2021-07-02 16:42:41 +0100

    add errorHandler function

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,13 +1,29 @@
-/* eslint-disable no-undef */
+import { infoMessage, errorMessage } from './logger.js'
 
-export const requestLogger = (req, res, next) => {
-	console.log('Method:', request.method)
-	console.log('Path:  ', request.path)
-	console.log('Body:  ', request.body)
-	console.log('---')
+const requestLogger = (req, res, next) => {
+	infoMessage('Method:', req.method)
+	infoMessage('Path:  ', req.path)
+	infoMessage('Body:  ', req.body)
+	infoMessage('---')
 	next()
 }
 
-export const unknownEndpoint = (req, res) => {
+const unknownEndpoint = (req, res) => {
 	res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (err, req, res, next) => {
+	errorMessage(err.message)
+	if (errorHandler === 'CastError') {
+		return res.status(400).send( { err: 'malformatted id' })
+	} else if ( err.name === 'ValidationError' ) {
+		return res.status(400).json({ err: err.message })
+	}
+	next(err)
+}
+
+export {
+	requestLogger,
+	unknownEndpoint,
+	errorHandler
 }
