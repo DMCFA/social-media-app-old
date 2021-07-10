1da380b7ea9f8dad8ef4766d0caa68bfda89cf35
Duarte Almeida <duartemalmeida@gmail.com>
2021-07-10 17:01:05 +0100

    client api commit

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Formik } from 'formik'
+
+function postForm() {
+    const initialValues = {
+        email: '',
+        password: ''
+    }
+    function onSubmit(values)
+}
