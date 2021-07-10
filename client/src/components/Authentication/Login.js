857658a8ff8cd01f33ca07a9dad1d7f051183c79
Duarte Almeida <duartemalmeida@gmail.com>
2021-07-10 17:56:38 +0100

    add formik form

--- client/src/components/Authentication/Login.js
+++ client/src/components/Authentication/Login.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import { useFormik } from 'formik'
+import { Formik } from 'formik'
 import * as yup from 'yup'
+import {
+    TextField
+} from '@material-ui/core'
 
 export default function Login() {
     return (
-        <div className='login-container'>
-            
+        <div>
+            <h1>Travel Connect</h1>
+            <Formik initialValues={{
+                email: '',
+                password: ''
+            }} onSubmit={(data) => {
+                console.log(data);
+            }}>
+                {({ values, handleChange, handleBlur, handleSubmit }) => (
+                    <form onSubmit={handleSubmit}>
+                        <TextField name='email' value={values.email} onChange={handleChange} onBlur={handleBlur} />
+                    </form>
+                )}
+            </Formik>
         </div>
 
     )
