173938a86836d668bcdcdb7263ebd703c93180d9
Duarte Almeida <duartemalmeida@gmail.com>
2021-07-17 12:47:23 +0100

    rebuilt form with react hook form library

--- client/src/components/Authentication/Login.js
+++ client/src/components/Authentication/Login.js
@@ -1,52 +1,54 @@
-import React from 'react'
-import { Formik, Form, Field, useField } from 'formik'
-import * as yup from 'yup'
-import {
-	TextField,
-    Button
-} from '@material-ui/core'
+import React, { useEffect } from "react"
+// import { useDispatch } from 'react-redux'
+import { useForm, useController, useFieldArray } from "react-hook-form"
+//import login reducer
 
 import useStyles from './styles'
 
-const CustomTextField = props => {
-    const [field, meta] = useField({ placeholder, ...props })
-    const errorText = meta.error && meta.touched ? meta.error : ''
-    return <TextField placeholder={placeholder} {...field} helperText={errorText} />
-}
-
 export default function Login() {
-
     const classes = useStyles()
 
-	return (
-		<div className={classes.body}>
-			<h1>Travel Connect</h1>
-			<Formik initialValues={{
-				email: '',
-				password: ''
-			}} onSubmit={(data, {setSubmitting, resetForm}) => {
-                setSubmitting(true)
-                //async
-                setSubmitting(false)
-			}}>
-				{({ values, isSubmitting }) => (
-					<Form>
-                        <div>
-                            <CustomTextField placeholder='Email' name='email' type='input' as={TextField}/>
-                        </div>
-                        <div>
-                            <CustomTextField placeholder='Password' name='password' type='input' as={TextField}/>
-                        </div>
-                        <div className={classes.bnt}>
-                            <Button disabled={isSubmitting} type='submit'>Submit</Button>
-                            <Button disabled={isSubmitting} type='submit'>Sign Up</Button>
-                            <Button disabled={isSubmitting} type='submit'>Forgot Password</Button>
-                        </div>
-						<pre>{JSON.stringify(values, 2, null)}</pre>
-					</Form>
-				)}
-			</Formik>
-		</div>
+    const {
+      register,
+      formState: { errors },
+      handleSubmit
+    } = useForm({
+      mode: "onChange"
+    });
+    const onSubmit = (data) => {
+      console.log((JSON.stringify(data)))
+    };
+  
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">Email</label>
+        <input
+          placeholder="Your email goes here"
+          {...register("email", {
+            required: "An email is required",
+            pattern: {
+              value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email address"
+            }
+          })}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+  
+        <label htmlFor="password">Password</label>
+        <input
+          placeholder="Your password goes here"
+          {...register("password", {
+            required: "A password is required",
+            minLength: {
+              value: 5,
+              message: "Password needs to have a minimum of 5 characters"
+            }
+          })}
+        />
+        {errors.password && <p>{errors.lastName.message}</p>}
+        <input type="submit" />
+      </form>
+    );
+  }
 
-	)
-}
+//connect to redux
