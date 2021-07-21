ce389fcc3d85ddf04ac8d18a9a3268270fc97ec1
Duarte Almeida <duartemalmeida@gmail.com>
2021-07-21 22:02:36 +0100

    reference user on schema

--- server/models/post.js
+++ server/models/post.js
@@ -3,31 +3,35 @@ import mongoose from 'mongoose'
 const { Schema } = mongoose
 
 const postSchema = new Schema({
-	title: String,
-	author: String,
-	body: String,
-	comments: [{
-		body: String,
-		date: Date
-	}],
-	date: {
-		type: Date,
-		default: new Date ()
-	},
-	keywords: [String],
-	imageFile: String,
-	likes: {
-		type: Number,
-		default: 0
-	}
+    title: String,
+    author: String,
+    body: String,
+    comments: [{
+        body: String,
+        date: Date
+    }],
+    date: {
+        type: Date,
+        default: new Date ()
+    },
+    keywords: [String],
+    imageFile: String,
+    likes: {
+        type: Number,
+        default: 0
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 })
 
 postSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString()
-		delete returnedObject._id,
-		delete returnedObject.__v
-	}
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id,
+        delete returnedObject.__v
+    }
 })
 
 export default mongoose.model('Post', postSchema)
