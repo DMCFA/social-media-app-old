9022eba4074bf28a5acf7111660372f47012340c
Duarte Almeida <duartemalmeida@gmail.com>
2021-07-21 22:00:16 +0100

    create user schema

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+import { mongoose } from 'mongoose'
+import { uniqueValidator } from 'mongoose-unique-validator'
+
+const { Schema } = mongoose
+Schema.plugin(uniqueValidator)
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        minlength: 3,
+        unique: true
+    },
+    passwordHash: String,
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ]
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id,
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+userSchema.plugin(uniqueValidator)
+
+export default mongoose.model('Post', userSchema)
