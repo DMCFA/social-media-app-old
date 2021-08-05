8c8bb3115a064530e31012ea48e7e902c344ff2c
Duarte Almeida <duartemalmeida@gmail.com>
2021-08-05 20:15:23 +0100

    change nav bar color

--- client/src/components/Posts/Header/styles.js
+++ client/src/components/Posts/Header/styles.js
@@ -2,9 +2,11 @@ import { makeStyles } from "@material-ui/core";
 
 export default makeStyles(() => ({
     header: {
-        backgroundColor: '#FFE194',
+        backgroundColor: '#CCA8E9',
         borderRadius: '20',
         margin: '4.15 vh',
         display: 'flex',
+        padding: '0.5rem',
+        fontVariant: 'small-caps',
     }
 }))
