ae736216fb527d806ba5e05bf8b33d9c622fde71
Duarte Almeida <duartemalmeida@gmail.com>
2021-08-06 20:53:16 +0100

    add Router, Switch and some routes to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import { 
+	BrowserRouter as Router,
+	Route,
+	Switch,
+ } from 'react-router-dom'
 
 import useStyles from './styles'
 import Login from './components/Authentication/Login'
+import Recovery from './components/Authentication/Recovery/Recovery'
 
 function App() {
 	const classes = useStyles()
 
 	return (
-		<div className={classes.body}>
-			<Login />
-		</div>
+		<Router>
+			<div className={classes.body}>
+				<Switch>
+					<Route exact path='/'>
+					<Login />
+					</Route>
+					<Route path='/recovery'>
+						<Recovery />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	)
 }
 
