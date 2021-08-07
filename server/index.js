579bb9a133723f4a4dd09f591df78d1acdeacd73
Duarte Almeida <duartemalmeida@gmail.com>
2021-08-07 17:35:17 +0100

    import usersrouter and invoke it

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import { MONGODB_CONNECTION, PORT } from './utils/config.js'
 import { requestLogger, unknownEndpoint, errorHandler } from './utils/middleware.js'
 import { infoMessage, errorMessage } from './utils/logger.js'
 
+import { usersRouter } from './routes/users'
+
 const app = express()
 
 // app.use(express.static('build'))
@@ -14,15 +16,18 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 mongoose.connect(MONGODB_CONNECTION, ({
-	useNewUrlParser: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
 }))
-	.then(() => app.listen(PORT, () => infoMessage(`server running on port: ${PORT}`)))
-	.catch((error) => errorMessage(error.message))
+    .then(() => app.listen(PORT, () => infoMessage(`server running on port: ${PORT}`)))
+    .catch((error) => errorMessage(error.message))
 
 
 app.use(requestLogger) // before routes
+
+app.use('/users', usersRouter)
+
 app.use(unknownEndpoint) // after routes
 app.use(errorHandler) // after routes
 
