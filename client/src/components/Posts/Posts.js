152e7b57671566de6abbf88052a023ee1339b187
Duarte Almeida <duartemalmeida@gmail.com>
2021-08-12 18:32:16 +0100

    add useEffect function and map the posts to render the Post component

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,11 +1,24 @@
 import React, {useEffect} from 'react'
+import postService from '../../services/posts'
 
 const Posts = () => {
 
+    const [posts, setPosts] = ([])
+
     useEffect(() => {
-        effect
-        return () => {
-            cleanup
-        }
-    }, [input])
+        postService
+        .getAll().then(posts => setPosts(posts))
+    }, [])
+
+    return(
+        <div>
+            {posts.map(post => 
+            <Post
+                key={post.id}
+                post={post}
+            />)}
+        </div>
+    )
 }
+
+export default Posts
