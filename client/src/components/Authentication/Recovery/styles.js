9cf4ca3235ac9721f249e255040aa500a75bb15f
Duarte Almeida <duartemalmeida@gmail.com>
2021-08-19 16:48:45 +0100

    add styles

--- client/src/components/Authentication/Recovery/styles.js
+++ client/src/components/Authentication/Recovery/styles.js
@@ -5,5 +5,22 @@ export default makeStyles(() => ({
         display: 'flex',
         flexFlow: 'column wrap',
         alignItems: 'center'
-    }
+    },
+    title: {
+        color: '#035397',
+        textAlign: 'center',
+        fontFamily: 'sans-serif',
+        fontSize: '3rem',
+        fontVariant: 'small-caps',
+        fontWeight: '600',
+        letterSpacing: '5px'
+    },
+    form: {
+        marginTop: '2rem',
+    },
+    email: {
+        padding: '5px',
+        marginBottom: '3px',
+        textAlign: "center"
+    },
 }))
