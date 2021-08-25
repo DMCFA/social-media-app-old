1dc909e7b33a65aa152d367404aa1c7690390f69
Duarte Almeida <duartemalmeida@gmail.com>
2021-08-25 20:52:33 +0100

    create posts display page

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,24 +1,38 @@
-import React, {useEffect} from 'react'
-import postService from '../../services/posts'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import useStyles from './styles'
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+    Paper
+} from '@material-ui/core'
 
-const Posts = () => {
+function Post({ post }) {
 
-    const [posts, setPosts] = ([])
-
-    useEffect(() => {
-        postService
-        .getAll().then(posts => setPosts(posts))
-    }, [])
+    const styles = useStyles()
 
     return(
-        <div>
-            {posts.map(post => 
-            <Post
-                key={post.id}
-                post={post}
-            />)}
+        <div className='container'>
+            <div className='post'>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>
+                                    <Link to={`posts/${post.id}`}>
+                                        <i>{post.title}</i>
+                                        <i>{post.author}</i>
+                                    </Link>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
         </div>
     )
-}
 
-export default Posts
+}
