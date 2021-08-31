a6c460fe0adfb6e31b0b0920f8612d495f5c96df
Duarte Almeida <duartemalmeida@gmail.com>
2021-08-31 20:17:02 +0100

    add hook state for redirect on submit

--- client/src/components/Authentication/Recovery/Recovery.js
+++ client/src/components/Authentication/Recovery/Recovery.js
@@ -1,54 +1,60 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom";
 import Header from '../../Posts/Header/Header'
+import Success from './Success/Success'
 
 import useStyles from './styles'
 
 import {
-    Typography,
     TextField,
     Button
 } from '@material-ui/core'
+import { Link, Redirect, Switch, useHistory, Route } from 'react-router-dom'
+
 
 const Recovery = () => {
 
     const classes = useStyles()
 
-    const [email, setEmail] = useState('')
+    const history = useHistory()
 
-    const handleSubmit = e => {
-        setEmail({value: e.target.value})
+    const [email, setEmail] = useState('')
+    const [showSuccess, setShowSuccess] = useState(false)
+
+    const handleSubmit = (e) => {
+        setEmail({ value: e.target.value });
+        setShowSuccess(true)
     }
-    
+
+
     return(
         <div className={classes.forget}>
             <Header />
 
             <div className={classes.flex}>
                 <h3 className={classes.title}>Enter your email below</h3>
-
-                <div className={classes.form}>
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <TextField
-                                className={classes.email}
-                                helperText='Your email goes here'
-                                id='email' value={email}
-                                onChange={({ target }) => setEmail(target.value)}
+                    <div className={classes.form}>
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <TextField
+                                    className={classes.email}
+                                    helperText='Your email goes here'
+                                    id='email' value={email}
+                                    onChange={({ target }) => setEmail(target.value)}
                                 />
                             <div>
-                                <Link to='Posts/Notification/Success' email={email}>
-                                    <Button
-                                        className={classes.button}
-                                        variant='contained'
-                                        color='primary'
-                                        type='submit' id='recovery'>   
-                                        Recover Your Password
-                                    </Button>
-                                </Link>
+                                <Button
+                                    className={classes.button}
+                                    variant='contained'
+                                    color='primary'
+                                    type='submit' 
+                                    id='recovery'
+                                > 
+                                    Recover Your Password
+                                </Button>
                             </div>
                         </div>
                     </form>
+                    {showSuccess && <Success email={email} />}
                 </div>
             </div>
         </div>
---
+++ client/src/components/Authentication/Recovery/Success/Success.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import useStyles from './styles'
+
+const Success = ({email}) => {
+
+    const classes = useStyles()
+
+    return(
+        <div className={classes.notification}>
+            <h2>We sent an email to {email} </h2>
+        </div>
+    )
+}
+
+export default Success
