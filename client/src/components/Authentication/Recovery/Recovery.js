51e49db0d40dab3715560af0944bc04b5e7420f1
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-01 23:13:58 +0100

    fix the email not passing as prop to success

--- client/src/components/Authentication/Recovery/Recovery.js
+++ client/src/components/Authentication/Recovery/Recovery.js
@@ -21,41 +21,50 @@ const Recovery = () => {
     const [showSuccess, setShowSuccess] = useState(false)
 
     const handleSubmit = (e) => {
-        setEmail({ value: e.target.value });
+        setEmail(e.target.email.value);
         setShowSuccess(true)
     }
 
 
-    return(
+    return (
         <div className={classes.forget}>
             <Header />
-
             <div className={classes.flex}>
-                <h3 className={classes.title}>Enter your email below</h3>
-                    <div className={classes.form}>
-                        <form onSubmit={handleSubmit}>
-                            <div>
-                                <TextField
-                                    className={classes.email}
-                                    helperText='Your email goes here'
-                                    id='email' value={email}
-                                    onChange={({ target }) => setEmail(target.value)}
-                                />
-                            <div>
-                                <Button
-                                    className={classes.button}
-                                    variant='contained'
-                                    color='primary'
-                                    type='submit' 
-                                    id='recovery'
-                                > 
-                                    Recover Your Password
-                                </Button>
-                            </div>
-                        </div>
-                    </form>
-                    {showSuccess && <Success email={email} />}
-                </div>
+                {showSuccess ? (
+                    <Success email={email} />
+                ) : (
+                    <div>
+                        <h3 className={classes.title}>
+                            Enter your email below
+                        </h3>
+                        <div className={classes.form}>
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <TextField
+                                        className={classes.email}
+                                        helperText="Your email goes here"
+                                        id="email"
+                                        value={email}
+                                        onChange={({ target }) =>
+                                            setEmail(target.value)
+                                        }
+                                    />
+                                    <div>
+                                        <Button
+                                            className={classes.button}
+                                            variant="contained"
+                                            color="primary"
+                                            type="submit"
+                                            id="recovery"
+                                        >
+                                            Recover Your Password
+                                        </Button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>{' '}
+                    </div>
+                )}
             </div>
         </div>
     )
