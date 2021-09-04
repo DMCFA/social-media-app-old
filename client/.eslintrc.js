6c642fcbdefb98436788c05cfd480ba52cd0ff68
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-04 20:39:47 +0100

    fix eslint and prettier

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,23 +1,12 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
+{
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      jsx: true,
-    },
-    sourceType: "module",
-  },
-  plugins: ["react", "prettier"],
-  rules: {
-    "prettier/prettier": "error",
-    "no-unused-vars": "off",
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "windows"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
-  },
-};
+  "extends": [
+    "plugin:react/recommended",
+    "react-app",
+    "plugin:prettier/recommended"
+  ]
+}
--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,37 +1,11 @@
-module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
+{
+	"env": {
+	  "node": true,
+	  "commonjs": true,
+	  "es2021": true
 	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 12,
-		'sourceType': 'module'
-	},
-	'rules': {
-		'indent': [
-			'error',
-			4
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		],
-		'eqeqeq': 'error',
-		'no-trailing-spaces': 'error',
-		'object-curly-spacing': [
-			'error', 'always'
-		],
-		'arrow-spacing': [
-			'error', { 'before': true, 'after': true }
-		]
+	"extends": ["plugin:node/recommended", "plugin:prettier/recommended"],
+	"rules": {
+	  "node/no-unsupported-features/es-syntax": "off"
 	}
-}
+  }
