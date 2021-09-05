d6a937e075c75aaffe3088348bf11b5e53413cd9
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-05 18:00:52 +0100

    deleted routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,31 @@
-import React from 'react'
-import { 
-	BrowserRouter as Router,
-	Route,
-	Switch,
- } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './app.css';
+import useStyles from './styles';
+import Login from './components/Authentication/Login';
+import SignUp from './components/Authentication/Sign Up/SignUp';
+import Recovery from './components/Authentication/Recovery/Recovery';
 
-import useStyles from './styles'
-import Login from './components/Authentication/Login'
-// import SignUp from './components/Authentication/Sign Up/SignUp'
-import Recovery from './components/Authentication/Recovery/Recovery'
-import Success from './components/Authentication/Recovery/Success/Success'
-import Posts from './components/Posts/Posts'
+function App() {
+    const classes = useStyles();
 
-function App({ user, email }) {
-	const classes = useStyles()
-
-	return (
-		<Router>
-			<div className={classes.body}>
-				<Switch>
-					<Route exact path='/'>
-						{/* {!user ? 
+    return (
+        <Router>
+            <div className={classes.body}>
+                <Switch>
+                    <Route exact path="/">
+                        {/* {!user ? 
 						<Login user={user}/> :
 						<Posts />}	 */}
-						<Login />
-					</Route>
-					<Route path='/recovery'>
-						<Recovery />
-					</Route>
-					{/* <Route path='/success'>
-						<Success/>
-					</Route> */}
-					{/* <Route path='/signup'>
-						<SignUp />
-					</Route> */}
-				</Switch>
-			</div>
-		</Router>
-	)
+                        <Login />
+                    </Route>
+                    <Route path="/signup">
+                        <SignUp />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
