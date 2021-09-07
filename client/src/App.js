7b16f5d3199ba385145b0db8f06f73df10690dfa
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-07 20:14:50 +0100

    remove all routes and import routes component

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './app.css';
+import Routes from './routes';
 import useStyles from './styles';
-import Login from './components/Authentication/Login';
-import SignUp from './components/Authentication/Sign Up/SignUp';
-import Recovery from './components/Authentication/Recovery/Recovery';
 
 function App() {
     const classes = useStyles();
 
-    return (
-        <Router>
-            <div className={classes.body}>
-                <Switch>
-                    <Route exact path="/">
-                        {/* {!user ? 
-						<Login user={user}/> :
-						<Posts />}	 */}
-                        <Login />
-                    </Route>
-                    <Route path="/signup">
-                        <SignUp />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+    return <Routes />;
 }
 
 export default App;
