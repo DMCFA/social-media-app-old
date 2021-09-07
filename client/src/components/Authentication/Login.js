7133ae46d316086c4ecbe848d624c64b15e4d8ce
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-07 20:18:05 +0100

    clean up code

--- client/src/components/Authentication/Login.js
+++ client/src/components/Authentication/Login.js
@@ -1,123 +1,99 @@
-import React, { useState } from 'react'
-import Header from '../Posts/Header/Header'
+import React, { useState } from 'react';
+import Header from '../Posts/Header/Header';
 import Notification from '../Posts/Notification/Notification';
-import loginService from '../../services/login'
+import loginService from '../../services/login';
 
-import {
-  Link,
-  useHistory
-} from "react-router-dom";
+import { Link, useHistory } from 'react-router-dom';
 
-import useStyles from './styles'
-import {
-  TextField,
-  Button,
-  Typography,
-} from '@material-ui/core'
-
+import useStyles from './styles';
+import { TextField, Button, Typography } from '@material-ui/core';
 
 const Login = () => {
+    const history = useHistory();
 
-  const history = useHistory()
+    const classes = useStyles();
 
-  const classes = useStyles()
-  
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-  const [errorMessage, setErrorMessage] = useState(null)
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState(null);
 
-  
-  const handleLogin = async (e) => {
-    e.preventDefault()
+    const handleLogin = (e) => {
+        e.preventDefault();
 
-    try {
-      const user = await loginService.login({
-        username, password
-      })
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (e) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 3000)
-    }
-  }
+        //user object
+        const user = { username, password };
+    };
 
+    const signUp = () => {
+        let path = '/signup';
+        history.push(path);
+    };
 
-  const handleSubmit = (e) => {
-    setUsername({value: e.target.value})
-    setPassword({value: e.target.value})
-    e.preventDefault()
-  }
+    return (
+        <div>
+            <Header />
 
-  const handleChange = (e) => {
-    setUsername({value: e.target.value})
-    setPassword({value: e.target.value})
-  }
+            <Notification message={errorMessage} />
 
-  const signUp = () => {
-    let path = '/signup'
-    history.push(path)
-  }
-
-  return(
-      <div>
-        <Header />
-
-        <Notification message={errorMessage} />
-
-        <div className={classes.flex}>
-          <h2 className={classes.title}>Login to Travel Connect</h2>
-          <div>
-          <form onSubmit={handleLogin}>
-            <div className={classes.form}>
-              <TextField
-                className={classes.username}
-                autoFocus
-                helperText='Your username goes here'
-                label='Username' id='username' value={username}
-                onChange={({ target }) => setUsername(target.value)}
-                />
+            <div className={classes.flex}>
+                <h2 className={classes.title}>Login to Travel Connect</h2>
+                <div>
+                    <form onSubmit={handleLogin}>
+                        <div className={classes.form}>
+                            <TextField
+                                className={classes.username}
+                                autoFocus
+                                helperText="Your username goes here"
+                                label="Username"
+                                id="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                className={classes.password}
+                                autoComplete="off"
+                                helperText="Your password goes here"
+                                label="Password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className={classes.forget}>
+                            <Typography>
+                                <Link to="/recovery" className={classes.link}>
+                                    Forgot my password
+                                </Link>
+                            </Typography>
+                        </div>
+                        <div>
+                            <Button
+                                className={classes.button}
+                                variant="contained"
+                                color="primary"
+                                type="submit"
+                                id="login"
+                            >
+                                Login
+                            </Button>
+                            <Button
+                                onClick={signUp}
+                                className={classes.button}
+                                variant="contained"
+                                color="secondary"
+                                type="submit"
+                                id="signup"
+                            >
+                                Sign Up
+                            </Button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div>
-              <TextField
-                className={classes.password}
-                autoComplete='off'
-                helperText='Your password goes here'
-                label='Password' id='password' value={password}
-                onChange={({ target }) => setPassword(target.value)}
-              />
-            </div>
-            <div className={classes.forget}>
-              <Typography>
-                <Link to='/recovery' className={classes.link}>Forgot my password</Link>
-              </Typography>
-            </div>
-            <div>
-              <Button
-                className={classes.button} 
-                variant="contained" 
-                color="primary" 
-                type='submit' id='login'>
-                Login
-              </Button>
-              <Button
-                onClick={signUp}
-                className={classes.button} 
-                variant="contained" 
-                color="secondary" 
-                type='submit' id='signup'>
-                Sign Up
-              </Button>
-            </div>
-          </form>
-          </div>
         </div>
-      </div>
-  )
-}
+    );
+};
 
-export default Login
+export default Login;
