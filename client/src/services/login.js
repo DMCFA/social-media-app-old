1e7a5fa25781d6206c7607347108113aaec3de64
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-16 21:59:56 +0100

    create user reducer

--- client/src/services/login.js
+++ client/src/services/login.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
-const baseUrl = '/login'
+import axios from 'axios';
+const baseUrl = '/login';
 
-const login = async credentials => {
-    const req = await axios.post(baseUrl, credentials)
-    return req.data
-}
+const login = async (credentials) => {
+    const req = await axios.post(baseUrl, credentials);
+    return req.data;
+};
 
-export default login
+export default login;
