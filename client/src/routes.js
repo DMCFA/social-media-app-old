cf7c9032bd922a5465b7007369d613d67a246a4a
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-18 11:47:32 +0100

    clean routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,17 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Authentication/Login';
 import SignUp from './components/Authentication/Sign Up/SignUp';
 import Recovery from './components/Authentication/Recovery/Recovery';
+import Home from './components/Posts/Home';
 
 const Routes = () => {
     return (
         <Router>
             <Switch>
-                <Route exact path="/" component={Login}>
-                    {/* {!user ? 
-                    <Login user={user}/> :
-                    <Posts />}	 */}
-                </Route>
+                <Route path="/login" component={Login}></Route>
                 <Route path="/signup" component={SignUp}></Route>
+                {/* <Route exact path="/" component={Home}></Route> */}
             </Switch>
         </Router>
     );
