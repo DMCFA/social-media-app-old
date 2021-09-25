01b6af82b200911189f9400eb08777b32c2cccfa
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-25 11:58:08 +0100

    start to redo header by adding other components to it

--- client/src/components/Posts/Header/Header.js
+++ client/src/components/Posts/Header/Header.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import {
-  AppBar,
-  Typography
-} from '@material-ui/core'
-import makeStyles from './styles'
+import { AppBar, Typography, InputBase, Link } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import makeStyles from './styles';
 
 const Header = () => {
-  const classes = makeStyles()
+    const classes = makeStyles();
 
-  return (
-      <AppBar className={classes.header} position='static'>
-        <Typography variant='h2' align='left'>
-          Travel Connect
-        </Typography>
-      </AppBar>
-  )
-}
+    return (
+        <AppBar className={classes.header} position="static">
+            <Typography
+                className={classes.headerTitle}
+                variant="h2"
+                align="left"
+            >
+                Travel Connect
+            </Typography>
+            <div className={classes.search}>
+                <SearchIcon fontSize="large" />
+            </div>
+            <InputBase
+                placeholder="Search…"
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                className={classes.searchField}
+                inputProps={{ 'aria-label': 'search' }}
+            />
+            <Link
+                className={classes.explore}
+                color="white"
+                underline="hover"
+                variant="button"
+            >
+                Explore
+            </Link>
+        </AppBar>
+    );
+};
 
-export default Header
+export default Header;
--- client/src/components/Posts/Header/styles.js
+++ client/src/components/Posts/Header/styles.js
@@ -1,12 +1,27 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core';
 
 export default makeStyles(() => ({
     header: {
-        backgroundColor: '#CCA8E9',
+        backgroundColor: '#FFB085',
         borderRadius: '20',
         margin: '4.15 vh',
         display: 'flex',
+        flexDirection: 'row',
         padding: '0.5rem',
         fontVariant: 'small-caps',
-    }
-}))
+    },
+    search: {
+        paddingInline: '25px',
+        marginTop: '25px',
+    },
+    searchField: {
+        marginTop: '15px',
+    },
+    explore: {
+        fontWeight: '600',
+        cursor: 'pointer',
+        fontSize: '18px',
+        letterSpacing: '1px',
+        marginBlockStart: '28px',
+    },
+}));
