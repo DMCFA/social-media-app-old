56e2299ada3af3cb408e78d821b7aac68c8b4d10
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-25 11:11:15 +0100

    redo home base structure

--- client/src/components/Posts/Home.js
+++ client/src/components/Posts/Home.js
@@ -15,27 +15,9 @@ import {
 const Home = () => {
     const styles = useStyles();
 
-    const post = useSelector((store) => store.posts);
-
     return (
         <div className="container">
             <Header />
-            <div className="post">
-                <TableContainer component={Paper}>
-                    <Table>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell>
-                                    <Link to={`posts/${post.id}`}>
-                                        <i>{post.title}</i>
-                                        <i>{post.author}</i>
-                                    </Link>
-                                </TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
         </div>
     );
 };
